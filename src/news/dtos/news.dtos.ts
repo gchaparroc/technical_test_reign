74c1a95441562b34a875993e4559d0b68f390832
gchaparroc <gchaparroc.certificaciones@gmail.com>
2022-07-21 07:47:51 -0400

    antes de implementar la base de datos

--- src/news/dtos/news.dtos.ts
+++ src/news/dtos/news.dtos.ts
@@ -1,8 +1,16 @@
-//import { PartialType } from '@nestjs/mapped-types';
+import { IsString } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+
 export class CreateNoticeDto {
+
+    @IsString()
     readonly title: string;
+
+    @IsString()
     readonly author: string;
+
+    @IsString()
     readonly comment_text: string;
   }
 
-  //export class UpdateProductDto extends PartialType(CreateProductDto) {}
+  export class UpdateNoticeDto extends PartialType(CreateNoticeDto) {}
