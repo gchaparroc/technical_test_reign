653cb8a93caa90b409907ad39f6ff79cd992653a
gchaparroc <gchaparroc.certificaciones@gmail.com>
2022-07-21 08:50:09 -0400

    creacion de modulo global para la BD

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NewsController } from './news/controllers/news.controller';
 import { NewsService } from './news/services/news.service';
+import { NewsModule } from './news/news.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [NewsModule, DatabaseModule],
   controllers: [AppController, NewsController],
   providers: [AppService, NewsService],
 })
