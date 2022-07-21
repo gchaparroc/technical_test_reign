336b61c6d728271d7e4a9622dfeca83e39953913
gchaparroc <gchaparroc.certificaciones@gmail.com>
2022-07-21 14:43:39 -0400

    instalando TypeOrm

--- src/news/entities/notice.entity.ts
+++ src/news/entities/notice.entity.ts
@@ -1,7 +1,17 @@
+import { PrimaryGeneratedColumn, Column, Entity } from 'typeorm';
+
+@Entity()
 export class Notice {
+    @PrimaryGeneratedColumn()
     id: number;
+
+    @Column({ type: 'varchar', length: 255 })
     title: string;
+
+    @Column({ type: 'varchar', length: 255 })
     author: string;
+
+    @Column({ type: 'text' })
     comment_text: string;
     /*_tags: number;*/
 }
